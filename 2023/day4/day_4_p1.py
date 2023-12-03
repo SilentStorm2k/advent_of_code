f8f3c916db493d0f6e916f913c239aa4dd82b87a
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2023-12-03 14:41:32 +0530

    fix: fixed boilerplate for day4

--- 2023/day4/day_4_p1.py
+++ 2023/day4/day_4_p1.py
@@ -4,8 +4,9 @@ def func(input):
     
     return 
 
-ex_file = open("advent_of_code/2023/day1/puzzle_input/day_3.txt", 'r')
-# ex_file = open("advent_of_code/2023/day1/puzzle_input/day_3_p1_example.txt", 'r')
+# when called from ~/Code/repos/advent_of_code$
+ex_file = open("2023/day4/puzzle_input/day_4.txt", 'r')
+# ex_file = open("2023/day4/puzzle_input/day_4_p1_example.txt", 'r')
 input = ex_file.read()
 # print(text)
 print(f"Answer is : {func(input)}")
