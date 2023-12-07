7de60c6ee74e9e69f7e96aaa8fef88e1e523be5d
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2023-12-07 23:33:33 +0530

    filemg: better bp for all days

--- 2023/day 17/sol.py
+++ 2023/day 17/sol.py
@@ -1,19 +1,23 @@
-import os
 import time
 
 def execute(func):
     def wrapper(*args):   
         t1 = time.time()
-        print(f'Answer is : {func(*args)}')
+        print(f'Answer for {func.__name__} : {func(*args)}')
         t2 = time.time()
         print(f'Executed in : {round(t2-t1, 5)}')
     return wrapper 
 
 @execute
-def func(input):
+def p1(input):
     return 
+
+@execute
+def p2(input):
+    return
 
 # when called from ~/Code/repos/advent_of_code$
 ex = 1
-input = open("2023/day 8/puzzle_input/day_8_example.txt" if ex else "2023/day 8/puzzle_input/day_8.txt", 'r').read()
-func(input)
+input = open("2023/day 17/puzzle_input/example.txt" if ex else "2023/day 17/puzzle_input/input.txt", 'r').read()
+p1(input)
+p2(input)
--- 2023/day 20/sol.py
+++ 2023/day 20/sol.py
@@ -3,16 +3,21 @@ import time
 def execute(func):
     def wrapper(*args):   
         t1 = time.time()
-        print(f'Answer is : {func(*args)}')
+        print(f'Answer for {func.__name__} : {func(*args)}')
         t2 = time.time()
         print(f'Executed in : {round(t2-t1, 5)}')
     return wrapper 
 
 @execute
-def func(input):
+def p1(input):
     return 
+
+@execute
+def p2(input):
+    return
 
 # when called from ~/Code/repos/advent_of_code$
 ex = 1
-input = open("2023/day 21/puzzle_input/example.txt" if ex else "2023/day 21/puzzle_input/input.txt", 'r').read()
-func(input)
+input = open("2023/day 20/puzzle_input/example.txt" if ex else "2023/day 20/puzzle_input/input.txt", 'r').read()
+p1(input)
+p2(input)
