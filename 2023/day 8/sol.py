3330b291a48f43494c313db885e462f3904c7a74
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2023-12-08 13:33:44 +0530

    refactor/docs: added comments and improved readability

--- 2023/day 8/sol.py
+++ 2023/day 8/sol.py
@@ -15,24 +15,22 @@ def execute(func):
 @execute
 def p1(input, start, end):
     ins, coords = input.split('\n\n')
-    coord = {re.findall(r'[A-Z0-9]+', cur)[0] : (re.findall(r'[A-Z0-9]+', cur)[1], re.findall(r'[A-Z0-9]+', cur)[2]) for cur in coords.split('\n')}
-    cur = start
-    index, steps = 0, 0
-    while (re.fullmatch(end, cur) == None):
-        direction = 1 if ins[index] == 'R' else 0
-        index += 1
-        index = 0 if index == len(ins) else index 
-        cur = coord[cur][direction]
+    coord = {c[0] : (c[1], c[2]) for cur in coords.split('\n') if (c := re.findall(r'[A-Z0-9]+', cur))}
+    cur, index, steps = start, 0, 0
+    while (re.fullmatch(end, cur) == None):             # while current not at end
+        direction = 1 if ins[index] == 'R' else 0       # for left or right
+        index += 1 if index != len(ins)-1 else -index   # increment or loopback
+        cur = coord[cur][direction]                     
         steps += 1
     return steps
 
 @execute
 def p2(input):
     ins, coords = input.split('\n\n')
-    coord = {re.findall(r'[A-Z0-9]+', cur)[0] : (re.findall(r'[A-Z0-9]+', cur)[1], re.findall(r'[A-Z0-9]+', cur)[2]) for cur in coords.split('\n')}
-    cur = [cor for cor in coord.keys() if re.fullmatch(r'(..A)', cor)]
-    min_steps = [p1.__original(input, c, r'(..Z)') for c in cur]
-    return math.lcm(*min_steps)
+    coord = {c[0] : (c[1], c[2]) for cur in coords.split('\n') if (c := re.findall(r'[A-Z0-9]+', cur))}
+    cur = [cor for cor in coord.keys() if re.fullmatch(r'(..A)', cor)]  # list of all possible starting coords
+    min_steps = [p1.__original(input, c, r'(..Z)') for c in cur]        # calculating steps for each starting coord
+    return math.lcm(*min_steps)                                         # total traversal len is the lcm of all traversal len
 
 # when called from ~/Code/repos/advent_of_code$
 ex = 0
