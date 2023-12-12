876cfcaaf465f35bf453eae4186535c25c6a608b
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2023-12-12 18:17:37 +0530

    sol: day 12 sol added (got help)
    used ideas and soution guide from jonathanpaulson

--- 2023/day 12/sol.py
+++ 2023/day 12/sol.py
@@ -1,4 +1,6 @@
 import time
+import functools
+import regex as re
 
 def execute(func):
     def wrapper(*args):   
@@ -11,14 +13,59 @@ def execute(func):
 
 @execute
 def p1(input):
-    return 
+    input = input.split('\n')
+    ways = 0
+    for line in input:
+        dots, blocks = line.split()
+        blocks = [int(x) for x in blocks.split(',')]
+        memo.clear()
+        ans = f(dots, blocks, 0, 0, 0)
+        ways += ans
+
+    return ways
 
 @execute
 def p2(input):
-    return
+    input = input.split('\n')
+    ways = 0
+    for line in input:
+        dots, blocks = line.split()
+        blocks = [int(x) for x in blocks.split(',')]
+        memo.clear()
+        dots = '?'.join([dots, dots, dots, dots, dots])
+        blocks = blocks*5
+        # print(f'{dots} {blocks}')
+        ways += f(dots, blocks, 0, 0, 0)
+
+    return ways
+
+memo = {}
+def f(dots, blocks, i, bi, current):
+    key = (i, bi, current)
+    if key in memo:
+        return memo[key]
+    if i == len(dots):
+        if bi == len(blocks) and current == 0:
+            return 1
+        elif bi == len(blocks)-1 and blocks[bi]==current:
+            return 1
+        else:
+            return 0
+    ans = 0
+    for c in ['.', '#']:
+        if dots[i] == c or dots[i] == '?':
+            if c == '.' and current == 0:
+                ans += f(dots, blocks, i+1, bi, 0)
+            elif c == '.' and current > 0 and bi<len(blocks) and blocks[bi]==current:
+                ans += f(dots, blocks, i+1, bi+1, 0)
+            elif c == '#':
+                ans += f(dots, blocks, i+1, bi, current+1)
+    memo[key] = ans
+    return ans
+
 
 # when called from ~/Code/repos/advent_of_code$
-ex = 1
+ex = 0
 input = open("2023/day 12/puzzle_input/example.txt" if ex else "2023/day 12/puzzle_input/input.txt", 'r').read()
 p1(input)
 p2(input)
