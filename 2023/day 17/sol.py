e9b39d915fe40f51077f8a38dc5277f6fec2318f
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2023-12-17 13:18:48 +0530

    sol: added day 17 solution

--- 2023/day 17/sol.py
+++ 2023/day 17/sol.py
@@ -1,4 +1,5 @@
 import time
+from heapq import heappop, heappush
 
 def execute(func):
     def wrapper(*args):   
@@ -10,15 +11,48 @@ def execute(func):
     return wrapper 
 
 @execute
-def p1(input):
-    return 
+def p1(input, mn = 0, mx = 3):
+    input = input.split('\n')
+    rows = [[] for _ in range(len(input))]
+    for r, row in enumerate(input):
+        rows[r] = [int(ele) for ele in row]
+    
+    R, C = len(rows)-1, len(rows[0])-1
+    seen = set()
+    pq = [(0, 0, 0, 0, 1, 0)]
+    minHeatLoss = 0
+    while pq:
+        hl, r, c, dr, dc, n = heappop(pq)
+        if r==R and c==C and n >= mn:
+            minHeatLoss = hl
+            break
+
+        if (r, c, dr, dc, n) in seen:
+            continue
+
+        seen.add((r, c, dr, dc, n))
+
+        if n < mx:
+            nr = r+dr
+            nc = c+dc
+            if 0<=nr<=R and 0<=nc<=C:
+                heappush(pq, (hl+rows[nr][nc], nr, nc, dr, dc, n+1))
+        
+        for ndr, ndc in [(0,1), (1,0), (-1,0), (0,-1)]:
+            if (ndr, ndc) != (-dr, -dc) and (ndr, ndc) != (dr, dc) and n >= mn:
+                nr = r+ndr
+                nc = c+ndc
+                if 0<=nr<=R and 0<=nc<=C:
+                    heappush(pq, (hl+rows[nr][nc], nr, nc, ndr, ndc, 1))
+    return minHeatLoss
+
 
 @execute
 def p2(input):
-    return
+    return p1.__original(input, 4, 10)
 
 # when called from ~/Code/repos/advent_of_code$
-ex = 1
+ex = 0
 input = open("2023/day 17/puzzle_input/example.txt" if ex else "2023/day 17/puzzle_input/input.txt", 'r').read()
 p1(input)
 p2(input)
