2c6a4d04a21f0b44e9f32dbec8772e4c70afcf9c
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2023-12-23 17:21:27 +0530

    sol: day 23 p1 sol added
    day 22 does not work neither does p2 day23

--- 2023/22.py
+++ 2023/22.py
@@ -11,7 +11,86 @@ def execute(func):
 
 @execute
 def p1(input):
-    return 
+    input = input.split('\n')
+    ends = []
+    for row in input:
+        t, b = row.split('~')
+        t = tuple([int(i) for i in t.split(',')])
+        b = tuple([int(i) for i in b.split(',')])
+        ends.append((t, b))
+    
+    # assuming that cubes extend towards the axises
+    sor = sorted(ends, key= lambda e: e[0][2])
+    fallenBlocks = []
+    flag = False
+
+    ### Probably somethign wrong here
+    for i in range(len(sor)):
+        current = sor[i]
+        flag = False
+        for j in range(len(fallenBlocks)-1, -1, -1):
+            below = fallenBlocks[j]
+            if flag: break
+            if isCollision(current, below):
+                botZ = below[1][2]+1
+                new = ((current[0][0], current[0][1], botZ), (current[1][0], current[1][1], current[1][2]-current[0][2]+botZ))
+                fallenBlocks.append(new)
+                flag = True
+        new = ((current[0][0], current[0][1], 1), (current[1][0], current[1][1], current[1][2]-current[0][2]+1))
+        if not flag: 
+            fallenBlocks.append(new)
+
+    sAbove = {}
+    # iterating over blocks, if there is a block immediately above cur
+    # and it will overlap if fallen, then cur supports it, add above to list of blocks supported by cur (sAbove)
+    for i in range(len(fallenBlocks)):
+        cur = fallenBlocks[i]
+        ab = []
+        for j in range(i+1, len(fallenBlocks)):
+            above = fallenBlocks[j]
+            if above[0][2] - cur[1][2] != 1:
+                continue
+            if isCollision(cur, above):
+                ab.append(j)
+        sAbove.update({i : ab})
+
+
+    # print(fallenBlocks)
+    # print(sAbove)
+    ans = 0
+    ans1 = 0
+    ans2 = 0
+    for b in range(len(fallenBlocks)):
+        # not supporting any blocks, hence can be disintegrated
+        if sAbove.get(b) == []:
+            ans += 1
+            ans1 += 1
+        else:
+            vals = sAbove.get(b)
+            count = [0 for _ in range(len(vals))]
+            for k in sAbove.keys():
+                for i, val in enumerate(vals):
+                    if val in sAbove.get(k):
+                        count[i] += 1
+            # if all blocks are supported, it can be disintegrated
+            if all(c >= 2 for c in count):
+                ans += 1
+                ans2 += 1
+
+    print(ans1)
+    print(ans2)
+    print(fallenBlocks)
+    print(sAbove)
+    return ans
+
+def isCollision(block1, block2):
+    b1X = (block1[0][0], block1[1][0])
+    b2X = (block2[0][0], block2[1][0])
+    b1Y = (block1[0][1], block1[1][1])
+    b2Y = (block2[0][1], block2[1][1])
+    intersectionX = max(b1X[0], b2X[0]) == min(b1X[1], b2X[1])
+    intersectionY = max(b1Y[0], b2Y[0]) == min(b1Y[1], b2Y[1])
+    return intersectionX and intersectionY
 
 @execute
 def p2(input):
@@ -19,6 +98,6 @@ def p2(input):
 
 # when called from ~/Code/repos/advent_of_code$
 ex = 1
-input = open("2023/day 22/puzzle_input/example.txt" if ex else "2023/day 22/puzzle_input/input.txt", 'r').read()
+input = open("2023/day 22/puzzle_input/example3.txt" if ex else "2023/day 22/puzzle_input/input.txt", 'r').read()
 p1(input)
 p2(input)
--- 2023/23.py
+++ 2023/23.py
@@ -1,4 +1,6 @@
 import time
+import sys
+from collections import deque
 
 def execute(func):
     def wrapper(*args):   
@@ -9,16 +11,74 @@ def execute(func):
     wrapper.__original = func # if need to reuse p1 w/o decorator use : p1.__original(input)
     return wrapper 
 
+
 @execute
 def p1(input):
-    return 
+    grid = []
+    # parsing input and putting it in grid
+    rows = input.split('\n')
+    for r in rows:
+        grid.append(list(r))
+    R, C = len(grid)-1, len(grid[0])-1
+    start = (0, 1)
+    end = (R, C-1)
 
+    sys.setrecursionlimit(2300)
+    longestPath = findPath(grid, start, end, set())
+    return longestPath-1
+
+
+slopes = {'>':(0,1), '<':(0,-1), 'v':(1,0), '^':(-1,0)}
+def findPath(grid, current, goal, seen):
+    R, C = len(grid)-1, len(grid[0])-1
+    if current == goal:
+        return 1
+    if current in seen:
+        return 0
+    x, y = current[0], current[1]
+    tile = grid[x][y]
+    # if out of bounds
+    if x >= R or x < 0 or y >= C or y < 0:
+        return 0
+    # on invalid tile
+    if tile == '#':
+        return 0
+    elif tile in slopes.keys():
+        seen.add(current)
+        dx, dy = slopes.get(tile)
+        # return findPath(grid, (x+dx, y+dy), goal, seen)
+        return 1+findPath(grid, (x+dx, y+dy), goal, seen)
+    else:
+        lengths = []
+        seen.add(current)
+        # bug for longest time : need to send new copy of seen for each path
+        newSeen = set(seen)
+        for dx, dy in [(0,1), (1,0), (0,-1), (-1, 0)]:
+            lengths.append(findPath(grid, (x+dx, y+dy), goal, newSeen))
+        return 1+max(lengths)
+    
 @execute
 def p2(input):
-    return
+    grid = []
+    # parsing input and putting it in grid
+    rows = input.split('\n')
+    for r in rows:
+        grid.append(list(r))
+    R, C = len(grid)-1, len(grid[0])-1
+    for r in range(R+1):
+        for c in range(C+1):
+            if grid[r][c] in slopes.keys():
+                grid[r][c] = '.'
+    s = ''
+    for r in range(R+1):
+        news = "".join(grid[r])
+        s = s+news+ ('\n' if r != R else '')
+    
+    return p1.__original(s)
+
 
 # when called from ~/Code/repos/advent_of_code$
-ex = 1
+ex = 0
 input = open("2023/day 23/puzzle_input/example.txt" if ex else "2023/day 23/puzzle_input/input.txt", 'r').read()
 p1(input)
 p2(input)
