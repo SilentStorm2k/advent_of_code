6c283452f0c8d330cc4eda15176fbb9defaad6ff
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2023-12-25 14:45:49 +0530

    sol: added day 25 solution
    needed to use external library for this (networkx is cool)

--- 2023/22.py
+++ 2023/22.py
@@ -24,7 +24,7 @@ def p1(input):
     fallenBlocks = []
     flag = False
 
-    ### Probably somethign wrong here
+    # simulate falling
     for i in range(len(sor)):
         current = sor[i]
         flag = False
@@ -36,23 +36,25 @@ def p1(input):
                 new = ((current[0][0], current[0][1], botZ), (current[1][0], current[1][1], current[1][2]-current[0][2]+botZ))
                 fallenBlocks.append(new)
                 flag = True
-        new = ((current[0][0], current[0][1], 1), (current[1][0], current[1][1], current[1][2]-current[0][2]+1))
         if not flag: 
+            new = ((current[0][0], current[0][1], 1), (current[1][0], current[1][1], current[1][2]-current[0][2]+1))
             fallenBlocks.append(new)
 
-    sAbove = {}
+    sAbove = {i:[] for i in range(len(fallenBlocks))}
+    # sAbove = {}
     # iterating over blocks, if there is a block immediately above cur
     # and it will overlap if fallen, then cur supports it, add above to list of blocks supported by cur (sAbove)
     for i in range(len(fallenBlocks)):
         cur = fallenBlocks[i]
-        ab = []
+        # ab = []
         for j in range(i+1, len(fallenBlocks)):
             above = fallenBlocks[j]
             if above[0][2] - cur[1][2] != 1:
                 continue
             if isCollision(cur, above):
-                ab.append(j)
-        sAbove.update({i : ab})
+                sAbove[i].append(j)
+                # ab.append(j)
+        # sAbove.update({i : ab})
 
 
     # print(fallenBlocks)
@@ -72,15 +74,19 @@ def p1(input):
                 for i, val in enumerate(vals):
                     if val in sAbove.get(k):
                         count[i] += 1
-            # if all blocks are supported, it can be disintegrated
+            # if all blocks are supported by at least one other block somewhere, it can be disintegrated
             if all(c >= 2 for c in count):
                 ans += 1
                 ans2 += 1
+    
+    print(get_safe_bricks(sAbove, fallenBlocks))
+    print(len(sAbove))
+    print(len(fallenBlocks))
 
-    print(ans1)
-    print(ans2)
-    print(fallenBlocks)
-    print(sAbove)
+    # print(ans1)
+    # print(ans2)
+    # print(fallenBlocks)
+    # print(sAbove)
     return ans
 
 def isCollision(block1, block2):
@@ -88,16 +94,25 @@ def isCollision(block1, block2):
     b2X = (block2[0][0], block2[1][0])
     b1Y = (block1[0][1], block1[1][1])
     b2Y = (block2[0][1], block2[1][1])
-    intersectionX = max(b1X[0], b2X[0]) == min(b1X[1], b2X[1])
-    intersectionY = max(b1Y[0], b2Y[0]) == min(b1Y[1], b2Y[1])
+    intersectionX = max(b1X[0], b2X[0]) == min(b1X[1], b2X[1]) or min(b1X[0], b2X[0]) == max(b1X[1], b2X[1])
+    intersectionY = max(b1Y[0], b2Y[0]) == min(b1Y[1], b2Y[1]) or min(b1Y[0], b2Y[0]) == max(b1Y[1], b2Y[1])
     return intersectionX and intersectionY
+
+def get_safe_bricks(support_stats, fallenBlocks):
+    can_be_disintegrated = 0
+    for brick_id, supporting_bricks in support_stats.items():
+       other_values = set([s for brick, supports in support_stats.items() for s in supports  if brick != brick_id])
+       if len(supporting_bricks) - len(other_values.intersection(set(supporting_bricks))) == 0:
+           can_be_disintegrated += 1
+    
+    return can_be_disintegrated + len(fallenBlocks) - len(support_stats)
 
 @execute
 def p2(input):
     return
 
 # when called from ~/Code/repos/advent_of_code$
-ex = 1
+ex = 0
 input = open("2023/day 22/puzzle_input/example3.txt" if ex else "2023/day 22/puzzle_input/input.txt", 'r').read()
 p1(input)
 p2(input)
--- 2023/25.py
+++ 2023/25.py
@@ -1,4 +1,5 @@
 import time
+import networkx as nx
 
 def execute(func):
     def wrapper(*args):   
@@ -11,14 +12,29 @@ def execute(func):
 
 @execute
 def p1(input):
-    return 
+    input = input.split('\n')
+    components = dict()
+    edges = nx.Graph()
+    for r in input:
+        connector, connections = r.strip().split(':')
+        connections = [c for c in connections.strip().split(' ')]
+        for connection in connections:
+            edges.add_edge(connector, connection)
+            edges.add_edge(connection, connector)
+        components.update({connector:connections})
+    
+    edges.remove_edges_from(nx.minimum_edge_cut(edges))
+    c = list(nx.connected_components(edges))
+    if len(c) == 2:
+        ans = len(c[0]) * len(c[1])
+    return ans
 
 @execute
 def p2(input):
     return
 
 # when called from ~/Code/repos/advent_of_code$
-ex = 1
+ex = 0
 input = open("2023/day 25/puzzle_input/example.txt" if ex else "2023/day 25/puzzle_input/input.txt", 'r').read()
 p1(input)
 p2(input)
