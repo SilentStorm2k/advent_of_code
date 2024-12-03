bc99e2d944a9482f70abd0aa7a41c259365f1f04
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-12-03 16:35:42 +0530

    feat: added boilerplate for dailies, and day4

---
+++ 2024/bp.py
@@ -0,0 +1,38 @@
+import time
+import re
+from collections import defaultdict, deque
+import heapq
+
+def execute(func):
+    def wrapper(*args):   
+        t1 = time.time()
+        print(f'Answer for {func.__name__} : {func(*args)}')
+        t2 = time.time()
+        print(f'Executed in : {round(t2-t1, 5)}')
+    wrapper.__original = func # if need to reuse p1 w/o decorator use : p1.__original(input)
+    return wrapper 
+
+
+@execute
+def p1(input):
+    return None
+                 
+@execute
+def p2(input):
+    return None
+ 
+
+
+def main():
+    # when called from ~/advent_of_code$
+    input = open("20xx/puzzle_input/00_input.txt", 'r').read()
+    example = open("20xx/puzzle_input/00_example.txt", 'r').read()
+    print("\nPart 1:")
+    p1(example)
+    p1(input)
+    print("\nPart 2:")
+    p2(example)
+    p2(input)
+
+if __name__ == "__main__":
+    main()
