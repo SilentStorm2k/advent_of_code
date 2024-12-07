01ffe805488f94dfd361449f1caf95ca7fb9b360
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-12-07 14:02:26 +0530

    feat: 06 part 2 solution added (p1 wrote cleaner)

--- 2024/06.py
+++ 2024/06.py
@@ -12,9 +12,47 @@ def execute(func):
     wrapper.__original = func # if need to reuse p1 w/o decorator use : p1.__original(input)
     return wrapper 
 
-
 @execute
 def p1(input):
+    grid, d, start = generateGrid(input)
+    path, _ = calculatePath(grid, d, start)
+    return len(set(path))
+
+@execute
+def p2(input):
+    grid, d, start = generateGrid(input)
+    path, cycle = calculatePath(grid, d, start)
+    blocks = set() 
+    for potentialBlock in path:
+        if potentialBlock == start or potentialBlock in blocks:
+            continue
+        i, j = potentialBlock
+        grid[i][j] = '#'
+        _, cycle = calculatePath(grid, d, start)
+        if cycle:
+            blocks.add((i,j))
+        grid[i][j] = '.'
+    return len(blocks) 
+ 
+def calculatePath (grid, d, start):
+    path = list()
+    seen = set((start, 0))
+    idx = 0
+    m, n = len(grid), len(grid[0])
+    pos = start
+    while 0<=pos[0]<m and 0<=pos[1]<n:
+        if grid[pos[0]][pos[1]] == '#':
+            pos = (pos[0]-d[idx][0], pos[1]-d[idx][1])
+            idx = (idx+1)%4
+        else:
+            if (pos, idx) in seen:
+                return [], True
+            seen.add((pos, idx))
+            path.append(pos)
+        pos = (pos[0]+d[idx][0], pos[1]+d[idx][1]) 
+    return path, False
+
+def generateGrid (input):
     d = [(-1,0), (0,1), (1,0), (0,-1)]
     grid = []
     pos = (0,0)
@@ -23,23 +61,7 @@ def p1(input):
         if '^' in row:
             pos = (i, row.index('^')) 
         grid.append(row)
-    m, n = len(grid), len(grid[0])
-    idx = 0
-    seen = set()
-    while 0<=pos[0]<m and 0<=pos[1]<n:
-        if grid[pos[0]][pos[1]] == '#':
-            pos = (pos[0]-d[idx][0], pos[1]-d[idx][1])
-            idx = (idx+1)%4
-        else:
-            seen.add(pos)
-        pos = (pos[0]+d[idx][0], pos[1]+d[idx][1]) 
-    return len(seen) 
-                 
-@execute
-def p2(input):
-    return None
- 
-
+    return grid, d, pos
 
 def main():
     # when called from ~/advent_of_code$
