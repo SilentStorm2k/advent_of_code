e5e0d97619d129ca394b9f9b835ee515d03207f3
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-12-13 18:44:37 +0530

    feat: cleaned day 13 solution added
    decided to hand solve linear equation (tried using lp solver i learnt from dsa course but p2 bad for it)

--- 2024/13.py
+++ 2024/13.py
@@ -16,73 +16,30 @@ def execute(func):
 
 
 @execute
-def p1(input):
+def p1(input, offset=0):
     res = 0
     for clawMachine in input.split('\n\n'):
         clawMachine = clawMachine.split('\n')
         buttonA, buttonB, prize = clawMachine
         buttonA = [int(ele.strip().split('+')[1]) for ele in buttonA.split(':')[1].split(',')]
         buttonB = [int(ele.strip().split('+')[1]) for ele in buttonB.split(':')[1].split(',')]
-        prize = [int(ele.strip().split('=')[1]) for ele in prize.split(':')[1].split(',')]
-
-        a = LpVariable("a", lowBound=0, cat='Integer')
-        b = LpVariable("b", lowBound=0, cat='Integer') 
+        prize = [int(ele.strip().split('=')[1])+offset for ele in prize.split(':')[1].split(',')]
+        x1, y1 = buttonA
+        x2, y2 = buttonB
+        p1, p2 = prize
         
-        prob = LpProblem("Minimize button presses", LpMinimize)
-        prob += 3*a+b
-        
-        # constraints
-        prob += a*buttonA[0] + b*buttonB[0] == prize[0]
-        prob += a*buttonA[1] + b*buttonB[1] == prize[1]
-        
-        prob.solve(PULP_CBC_CMD(msg=0))
-        # pulp.PULP_CBC_CMD(msg=False).solve(prob)
-        if LpStatus[prob.status] == "Optimal":
-            res += int(value(prob.objective))
-
+        # linear equation in 2 variables (can use substitution method as shown below) 
+        b = (p2*x1-p1*y1)/(x1*y2-x2*y1)
+        a = (p1-b*x2)/x1
+        if a == int(a) and b == int(b):
+            # only add if we get integer solution
+            res += int(3*a+b)
+ 
     return res 
                  
 @execute
 def p2(input):
-    res = 0
-    for clawMachine in input.split('\n\n'):
-        clawMachine = clawMachine.split('\n')
-        buttonA, buttonB, prize = clawMachine
-        buttonA = [int(ele.strip().split('+')[1]) for ele in buttonA.split(':')[1].split(',')]
-        buttonB = [int(ele.strip().split('+')[1]) for ele in buttonB.split(':')[1].split(',')]
-        prize = [int(ele.strip().split('=')[1]) + 10000000000000 for ele in prize.split(':')[1].split(',')]
-
-        buttons = np.array([buttonA, buttonB])
-        prize   = np.array(prize)
-        
-        try:
-            inverse = np.linalg.inv(buttons)
-        except np.linalg.LinAlgError:
-            continue
-
-        potRes = inverse @ prize 
-        # print(potRes)
-        res += 3*potRes[0] + potRes[1]
-
-
-        # print(buttonA, buttonB, prize)
-        # a = LpVariable("a", lowBound=0, cat='Integer')
-        # b = LpVariable("b", lowBound=0, cat='Integer') 
-        
-        # prob = LpProblem("Minimize button presses", LpMinimize)
-        # prob += 3*a+b
-        # # constraints
-        # prob += a*buttonA[0] + b*buttonB[0] == prize[0]
-        # prob += a*buttonA[1] + b*buttonB[1] == prize[1]
-        
-        # prob.solve(PULP_CBC_CMD(msg=0))
-        # # pulp.PULP_CBC_CMD(msg=False).solve(prob) 
-        # # print(LpStatus[prob.status]) 
-        # # print(value(a), value(b))
-        # if LpStatus[prob.status] == "Optimal":
-        #     res += int(value(prob.objective))
-
-    return res 
+    return p1.__original(input, offset=10000000000000)
 
 
 def main():
@@ -91,7 +48,7 @@ def main():
     example = open("2024/puzzle_input/13_example.txt", 'r').read()
     print("\nPart 1:")
     p1(example)
-    # p1(input)
+    p1(input)
     print("\nPart 2:")
     p2(example)
     p2(input)
